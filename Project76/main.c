25a5292559320c00420d2f45a5421cf70a994cbe
tjdud <tjdud@DESKTOP-7KJFUDJ>
2023-11-25 01:26:42 +0900

    assign

--- Project76/main.c
+++ Project76/main.c
@@ -4,7 +4,6 @@
 #define MAX_SIZE 20
 #define SWAP(x,y,t) ((t)=(x), (x)=(y), (y)=(t))
 
-int list[MAX_SIZE];
 int n;
 int move, compare;
 
@@ -17,43 +16,46 @@ void print_array(int arr[], int size) { //과정 출력
 }
 
 void bubble(int list[], int n) {
-    int i, j,temp;
-    move = 0;
-    compare = 0;
-
-    for (i = n - 1; i > 0; i--) {
-        for (j = 0; j < i; j++) {
-            compare += 1;
-            if (list[j] > list[j + i]) {
-                SWAP(list[j], list[j + 1], temp);
-                move += 1;
-                print_array(list, n);
-            }
-        }
-   }
-}
-
-void bubble2(int list[], int n) {
     int i, j, temp;
     move = 0;
     compare = 0;
 
     for (i = n - 1; i > 0; i--) {
         for (j = 0; j < i; j++) {
-            compare += 1;
-            if (list[j] > list[j + i]) {
+            compare += 1;  // 한 칸씩 뒤로 가며 한 번씩 비교
+            if (list[j] > list[j + 1]) {
                 SWAP(list[j], list[j + 1], temp);
-                move += 1;
-                //print_array(list, n);
+                move += 1;  // 비교 후 조건 달성 시 이동
             }
         }
+        print_array(list, n);  // 배열 전체 순회마다 1회 출력
     }
 }
+
+
+void bubble2(int list[], int n) { // 중간 과정 출력 생략
+    int i, j, temp;
+    move = 0;
+    compare = 0;
+
+    for (i = n - 1; i > 0; i--) {
+        for (j = 0; j < i; j++) {
+            compare += 1;  
+            if (list[j] > list[j + 1]) {
+                SWAP(list[j], list[j + 1], temp);
+                move += 1;  
+            }
+        }
+       //print_array(list, n);  
+    }
+}
+
 int main(void) {
     int i, j;
     int move_count = 0;
     int compare_count = 0;
     n = MAX_SIZE;
+    int list[MAX_SIZE];
 
     srand(time(NULL));
     for (i = 0; i < n; i++)
@@ -65,28 +67,21 @@ int main(void) {
     printf("Bubble Sort\n");
     bubble(list, n);
 
-    printf("Move Count: %d\n", move);
-    printf("Compare Count: %d\n\n", compare);
-
     move_count += move;
     compare_count += compare;
 
-    for (j = 1; j <= 19; j++) {
+    for (j = 1; j <= 19; j++) { //19번 반복
         for (i = 0; i < n; i++)
             list[i] = rand() % 100;
 
-        printf("Original list\n");
-        print_array(list, n);
-
-        printf("BUbble Sort\n");
+        
         bubble2(list, n);
-        print_array(list, n);
 
         move_count += move;
         compare_count += compare;
-        printf("\n");
 
     }
+    //평균
     printf("Average Move Count: %d\n", move_count / 20);
     printf("Average Compare Count: %d\n", compare_count / 20);
 
